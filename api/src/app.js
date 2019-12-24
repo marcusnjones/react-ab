840d6a52f186586b69bb0ed99a2c87ca9d8cb279
Marcus Jones <marcusnjones@gmail.com>
2019-12-23 19:22:13 -0500

    Add error handling

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const entry = require('./routes/entry');
 const entries = require('./routes/entries');
 
+app.use(morgan('dev'));
+
 app.use('/entry', entry);
 app.use('/entries', entries);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found!');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
