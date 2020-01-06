c3cc5dcfba2b13871c8dffccef300823ef14d4af
Marcus Jones <marcusnjones@gmail.com>
2020-01-05 22:14:54 -0500

    Code cleanup

--- api/src/app.js
+++ api/src/app.js
@@ -48,11 +48,11 @@ app.use((req, res, next) => {
     next(error);
 });
 
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
     res.json({
         error: {
-            message: error.message
+            message: err.message
         }
     });
 });
