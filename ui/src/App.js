6dfcbbe46683abb7d63cc909d2807a117f91cd1c
Marcus Jones <marcusnjones@gmail.com>
2020-04-04 02:11:23 -0400

    Initial commit for markup boilerplate

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,24 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="app-header">
+        <div className="search-container">
+          <label htmlFor="entry-search">Search:&nbsp;</label>
+          <input id="entry-search" type="search" name="entry-search" autoFocus />
+        </div>
+
+        <div className="logo-container">
+          <h2 className="logo">react-ab</h2>
+        </div>
       </header>
+
+      <main className="app-body">
+        <table id="entries">
+          <thead>
+            <tr>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone #</th>
+              <th scope="col">Address</th>
+              <th scope="col">City</th>
+              <th scope="col">State</th>
+              <th scope="col">ZIP</th>
+              <th scope="col">Updated</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>John</td>
+              <td>Doe</td>
+              <td>john.doe@email.com</td>
+              <td>(555) 555-5555</td>
+              <td>1234 Maple Street</td>
+              <td>Los Angeles</td>
+              <td>CA</td>
+              <td>90210</td>
+              <td>4/3/2020, 11:12:55 PM</td>
+              <td>
+                <ul className="action-list">
+                  <li>Edit</li>
+                  <li>Favorite</li>
+                  <li>Delete</li>
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>Jane</td>
+              <td>Doe</td>
+              <td>jane.doe@email.com</td>
+              <td>(666) 666-6666</td>
+              <td>4567 Peanut Street</td>
+              <td>New York</td>
+              <td>NY</td>
+              <td>10011</td>
+              <td>4/3/2020, 11:12:55 PM</td>
+              <td>
+                <ul className="action-list">
+                  <li>Edit</li>
+                  <li>Favorite</li>
+                  <li>Delete</li>
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 }
