6e9b16080b44105dc438ecbf0afc393c89747932
Marcus Jones <marcusnjones@gmail.com>
2020-10-13 22:22:08 -0400

    Add updateEntry resolver

--- api/src/resolvers/index.js
+++ api/src/resolvers/index.js
@@ -2,6 +2,24 @@ const mongoose = require('mongoose');
 const Entry = require('./../models/entry');
 
 module.exports = {
+  entry: async (arg) => {
+    const id = arg.id;
+
+    try {
+      const entry = await Entry
+      .findById(id);
+      if (entry) {
+        return { ...entry._doc };
+      }
+      else {
+        return entry;
+      }
+    }
+    catch (err) {
+      console.log(err);
+      throw err;
+    }
+  },
   entries: async () => {
     try {
       const entries = await Entry
@@ -16,23 +34,25 @@ module.exports = {
       }
     }
     catch (err) {
+      console.log(err);
       throw err;
     }
   },
   createEntry: async (arg) => {
     const entry = new Entry({
       _id: new mongoose.Types.ObjectId(),
-      _dateCreated: new Date(arg.entryInput._dateCreated),
+      _dateCreated: new Date(),
       dateUpdated: null,
-      firstName: arg.entryInput.firstName,
-      lastName: arg.entryInput.lastName,
-      email: arg.entryInput.email,
-      phone: arg.entryInput.phone,
-      address: arg.entryInput.address,
-      city: arg.entryInput.city,
-      state: arg.entryInput.state,
-      zip: arg.entryInput.zip
+      firstName: arg.data.firstName,
+      lastName: arg.data.lastName,
+      email: arg.data.email,
+      phone: arg.data.phone,
+      address: arg.data.address,
+      city: arg.data.city,
+      state: arg.data.state,
+      zip: arg.data.zip
     });
+
     try {
       const createdEntry = await entry.save();
       console.log('Entry created for:', createdEntry.firstName + ' ' + createdEntry.lastName);
@@ -41,6 +61,33 @@ module.exports = {
     catch (err) {
       console.log(err);
       throw err;
+    }
+  },
+  updateEntry: async (arg) => {
+    const id = arg.data._id,
+    entry = await Entry
+    .findById(id);
+    let updates = { 'dateUpdated': new Date() };
+
+    for (const [key, value] of Object.entries(arg.data)) {
+      if (typeof value == 'undefined') {
+        return;
+      }
+      if (Object.values(entry._doc).indexOf(value) == -1) {
+        if (key != '_id') {
+          updates[key] = value;
+        }
+      }
+    }
+    if (Object.keys(updates).includes('dateUpdated') && Object.entries(updates).length == 1) {
+      console.log('No fields updated for: ' + id);
+    } else {
+      let size = Object.keys(updates).length - 1;
+
+      await Entry
+      .updateOne({ '_id': id }, { $set: updates }, () => {
+        console.log('[' + size + '] field(s) updated for: ' + id);
+      });
     }
   }
 };
