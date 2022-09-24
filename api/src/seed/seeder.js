f7f91ca6a8a10c8a8deebb772f3eb130327584bb
Marcus Jones <marcusnjones@gmail.com>
2022-09-23 23:16:58 -0400

    Update seeder.js
    Prevents the seeder from running if entries already exist

--- api/src/seed/seeder.js
+++ api/src/seed/seeder.js
@@ -7,30 +7,39 @@ const data = JSON.parse(fs.readFileSync('./seed/MOCK_DATA.json'));
  * Defines the function that generates mock data for the database.
  * @return {void}
  */
-function seed() {
-  const entries = data.map((entry) => new Entry({
-    _id: new mongoose.Types.ObjectId(),
-    _dateCreated: new Date(),
-    dateUpdated: null,
-    firstName: entry.firstName,
-    lastName: entry.lastName,
-    email: entry.email,
-    phone: entry.phone,
-    address: entry.address,
-    city: entry.city,
-    state: entry.state,
-    zip: entry.zip,
-  }));
-
-  let done = 0;
-  for (let i = 0; i < entries.length; i++) {
-    entries[i].save((err, result) => {
-      console.log(`Entry created for: ${entries[i].firstName} ${entries[i].lastName}`);
-      done++;
-      if (done === entries.length) {
-        console.log('Seeding has completed!');
+async function seed() {
+  try {
+    const existingEntries = await Entry.find();
+    if (existingEntries.length == 0) {
+      const newEntries = data.map((entry) => new Entry({
+        _id: new mongoose.Types.ObjectId(),
+        _dateCreated: new Date(),
+        dateUpdated: null,
+        firstName: entry.firstName,
+        lastName: entry.lastName,
+        email: entry.email,
+        phone: entry.phone,
+        address: entry.address,
+        city: entry.city,
+        state: entry.state,
+        zip: entry.zip,
+      }));
+      
+      let done = 0;
+      for (let i = 0; i < newEntries.length; i++) {
+        newEntries[i].save((err, result) => {
+          console.log(`Entry created for: ${newEntries[i].firstName} ${newEntries[i].lastName}`);
+          done++;
+          if (done === newEntries.length) {
+            console.log('Seeding has completed!');
+          }
+        });
       }
-    });
+    }
+  }
+  catch (err) {
+    console.log(err);
+    throw err;
   }
 }
 
